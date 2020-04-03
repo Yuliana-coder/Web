893f934dbc470b41e67d722c5273b6bb7f2bc613
admin <antipina.yuliana@mail.ru>
2020-04-03 15:57:47 +0500

    выход из карточке по нажатию на свободное поле

--- 2дз/js/card.js
+++ 2дз/js/card.js
@@ -2,6 +2,7 @@ let card = document.querySelectorAll(".item-card");
 const button = document.querySelector(".click-card__btn-exit");
 const wind = document.querySelector(".hide-card");
 let wind_x = 0, wind_y = 0;
+const hidden_bloc = document.querySelector(".hidden-off");
 
 for (var i = 0; i < card.length; i++){
     card[i].addEventListener("click", showTargets);
@@ -19,6 +20,8 @@ function showTargets( event ) {
   clear_bloc(); 
   wind.classList.remove("hide-card");
   wind.classList.add("click-card");
+  hidden_bloc.classList.remove("hidden-off");
+  hidden_bloc.classList.add("hidden-on");
   const status = event.currentTarget.dataset.online;
   const photo = event.currentTarget.dataset.photo;
   const name_ = event.currentTarget.dataset.name;
@@ -35,9 +38,12 @@ function showTargets( event ) {
 }
 
 button.addEventListener("click", closeCard);
+hidden_bloc.addEventListener("click", closeCard);
 
 function closeCard(event){
     wind.classList.remove("click-card");
     wind.classList.add("hide-card");
+    hidden_bloc.classList.remove("hidden-on");
+    hidden_bloc.classList.add("hidden-off");
     clear_bloc();
 }
