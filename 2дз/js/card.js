fc64e606cecce3d718c78a54862dd1684bc42c40
admin <antipina.yuliana@mail.ru>
2020-04-03 15:24:10 +0500

    card_exit_with_button

--- 2дз/js/card.js
+++ 2дз/js/card.js
@@ -1,13 +1,22 @@
-const card = document.querySelectorAll(".item-card");
-const button = document.querySelector(".btn-exit");
-let wind = document.querySelector(".click-card");
+let card = document.querySelectorAll(".item-card");
+const button = document.querySelector(".click-card__btn-exit");
+const wind = document.querySelector(".hide-card");
+let wind_x = 0, wind_y = 0;
 
 for (var i = 0; i < card.length; i++){
     card[i].addEventListener("click", showTargets);
 }
 
+function clear_bloc(){
+    wind.querySelector(".info-card__status").innerHTML = '';
+    wind.querySelector(".info-card__name").innerHTML = '';
+    wind.querySelector(".info-card__birthday").innerHTML = '';
+    wind.querySelector(".info-card__number").innerHTML = ' ';
+    wind.querySelector(".click-card__photo").src = "";
+}
+
 function showTargets( event ) {
-clear_bloc();   
+  clear_bloc(); 
   wind.classList.remove("hide-card");
   wind.classList.add("click-card");
   const status = event.currentTarget.dataset.online;
@@ -15,22 +24,17 @@ clear_bloc();
   const name_ = event.currentTarget.dataset.name;
   const birthday = event.currentTarget.dataset.birthday;
   const number = event.currentTarget.dataset.number;
-  wind.querySelector(".click-card__status").innerHTML += status;
-  wind.querySelector(".click-card__name").innerHTML += name_;
-  wind.querySelector(".click-card__birthday").innerHTML += birthday;
-  wind.querySelector(".click-card__number").innerHTML += number;
+  wind.querySelector(".info-card__status").innerHTML += status;
+  wind.querySelector(".info-card__name").innerHTML += name_;
+  wind.querySelector(".info-card__birthday").innerHTML += birthday;
+  wind.querySelector(".info-card__number").innerHTML += number;
   wind.querySelector(".click-card__photo").src = photo;
+  console.log(wind_x);
+  console.log(wind_y);
+  console.log(event.currentTarget);
 }
 
 button.addEventListener("click", closeCard);
-
-function clear_bloc(){
-    wind.querySelector(".click-card__status").innerHTML = 'статус: ';
-    wind.querySelector(".click-card__name").innerHTML = 'Имя: ';
-    wind.querySelector(".click-card__birthday").innerHTML = 'День рождения: ';
-    wind.querySelector(".click-card__number").innerHTML = 'Телефон: ';
-    wind.querySelector(".click-card__photo").src = "";
-}
 
 function closeCard(event){
     wind.classList.remove("click-card");
